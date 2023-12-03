1cad428f13a52e1fad78c97f7caef57db2cee192
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2023-12-03 16:31:22 +0100

    LGBT dataset

--- src/main/java/de/citec/etradis/utils/LgbtDataReader.java
+++ src/main/java/de/citec/etradis/utils/LgbtDataReader.java
@@ -50,13 +50,9 @@ public class LgbtDataReader {
                 Element recordElement = (Element) recordList.item(i);
                 String recordId = recordElement.getElementsByTagName("id").item(0).getTextContent();
                 Record record = new Record(recordElement);
-                //System.out.println("attribute::"+record.getAttributes());
-                //System.out.println("detail attribute::"+record.getDetailAttributes());
                 String uri = "<http://localhost:9999/etradis#record" + recordId + ">";
                 String className = findEtradisClass(record.getAttributes().get("type"));
-                String mainTriple = getBasicTriple(uri, className);
-                String triple = mainTriple + getProperties(uri, record.getAttributes());
-                //relationship
+                String triple = getBasicTriple(uri, className) + getProperties(uri, record.getAttributes());
                 content += triple;
             }
         } catch (Exception e) {
@@ -73,31 +69,6 @@ public class LgbtDataReader {
         return typeFile;
     }
 
-   
-    public static Map<String, String> findDetail(Element detailElement) {
-        // String xmlString = "<detail conceptID=\"2-1\" name=\"Title of magazine\" id=\"1\" basename=\"Name or Title (please rename appropriately)\">Schwuchtel - Eine Zeitung der Schwulenbewegung</detail>";
-        Map<String, String> attributes = new HashMap<String, String>();
-        try {
-            String conceptID = detailElement.getAttribute("conceptID");
-            String name = detailElement.getAttribute("name");
-            String id = detailElement.getAttribute("id");
-            String basename = detailElement.getAttribute("basename");
-            String textContent = detailElement.getTextContent();
-
-            /*System.out.println("conceptID: " + conceptID);
-            System.out.println("name: " + name);
-            System.out.println("id: " + id);
-            System.out.println("basename: " + basename);
-            System.out.println("textContent: " + textContent);*/
-            attributes.put("conceptID", id);
-            attributes.put("name", name);
-            attributes.put("basename", basename);
-            attributes.put("textContent", textContent);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return attributes;
-    }
 
     public static void main(String[] args) {
         String srcDir = "src/main/resources/";
@@ -149,8 +120,12 @@ public class LgbtDataReader {
             value = value.replace("\n", " ");
             if (value.contains("http")) {
                 value = value.replace("https", "http");
-                content += uri + " <http://localhost:9999/" + attibute + "> " + "<" + value + ">" + " . " + "\n";
-            } else {
+                content += uri + " "+"<"+"http://localhost:9999/" + attibute + "> " + "<" + value + ">" + " . " + "\n";
+            }
+            else if(attibute.equals("relationship")){
+                content += uri + " "+"<"+"http://dbpedia.org/ontology/wikiPageWikiLink"+ ">"+" " + "<" +"http://localhost:9999/etradis#record" + value + ">" + " . " + "\n";
+            }
+            else {
                 if (attibute.contains("title")) {
                     content += uri + " "+"<"+"http://www.w3.org/2000/01/rdf-schema#label"+">"+" " + "\"" + value + "\"^^" + object + " . " + "\n";
                     content += uri + " "+"<"+"http://localhost:9999/" + attibute + "> " + "\"" + value + "\"^^" + object + " . " + "\n";
@@ -206,74 +181,4 @@ public class LgbtDataReader {
         return str;
     }
     
-     public static void mainT(String[] args) {
-        String xmlString = "<record visibility=\"viewable\" visnote=\"logged in users\" selected=\"no\" depth=\"0\">\n"
-                + "    <!-- other XML elements here -->\n"
-                + "    <id>2533</id>\n"
-                + "    <type id=\"80\" conceptID=\"3-124\">Magazine</type>\n"
-                + "    <citeAs>https://heurist.sfb1288.uni-bielefeld.de/heurist/?recID=2533&amp;db=ipaetzold_erste_datenbank</citeAs>\n"
-                + "    <title>Schwuchtel - Eine Zeitung der Schwulenbewegung</title>\n"
-                + "    <added>2023-05-01 15:54:06</added>\n"
-                + "    <modified>2023-05-01 15:55:00</modified>\n"
-                + "    <workgroup id=\"0\">public</workgroup>\n"
-                + "    <detail conceptID=\"2-1\" name=\"Title of magazine\" id=\"1\" basename=\"Name or Title (please rename appropriately)\">Schwuchtel - Eine Zeitung der Schwulenbewegung</detail>\n"
-                + "    <detail conceptID=\"2-134\" name=\"Location (place)\" id=\"134\" basename=\"Location (place)\" isRecordPointer=\"true\">2263</detail>\n"
-                + "    <detail conceptID=\"3-1017\" name=\"Publisher &gt;\" id=\"1001\" basename=\"Publisher\" isRecordPointer=\"true\">2414</detail>\n"
-                + "    <detail conceptID=\"2-36\" id=\"36\" name=\"Original ID\" basename=\"Original ID\">0-2533</detail>\n"
-                + "</record>";
-        Map<String, String> attributes = new HashMap<String, String>();
-
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(new InputSource(new StringReader(xmlString)));
-
-            Element recordElement = document.getDocumentElement();
-            NodeList nodeList = recordElement.getChildNodes();
-
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                Map<String, String> detailAttributes = new HashMap<String, String>();
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    String nodeName = element.getNodeName();
-                    String nodeValue = element.getTextContent();
-                    if (nodeName.contains("detail")) {
-                        detailAttributes = findDetail(element);
-                    } else {
-                        attributes.put(nodeName, nodeValue);
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        for (String key : attributes.keySet()) {
-            String value = attributes.get(key);
-            System.out.println(key + " " + value);
-
-        }
-    }
-
-    
-
-
 }
-
-
-/*// Extract information from the <record> element
-                String recordId = recordElement.getElementsByTagName("id").item(0).getTextContent();
-                String recordType = recordElement.getElementsByTagName("type").item(0).getTextContent();
-                String recordTitle = recordElement.getElementsByTagName("title").item(0).getTextContent();
-                String recordCiteAs = recordElement.getElementsByTagName("citeAs").item(0).getTextContent();
-                 // Print information about the record
-                System.out.println("Record ID: " + recordId);
-                System.out.println("Record Type: " + recordType);
-                System.out.println("Record Title: " + recordTitle);
-                System.out.println("Record CiteAs: " + recordCiteAs);
-                System.out.println("---------------------------------------");
-                
-                                //String triple = uri+" <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/"+className+">" + " . " + "\n";
-
-               NodeList detailList = recordElement.getChildNodes();*/
--- src/main/java/de/citec/etradis/utils/Record.java
+++ src/main/java/de/citec/etradis/utils/Record.java
@@ -6,6 +6,7 @@
 package de.citec.etradis.utils;
 
 import java.io.StringReader;
+import static java.lang.System.exit;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,113 +26,78 @@ import org.xml.sax.InputSource;
 public class Record {
 
     private Map<String, String> attributes = new HashMap<String, String>();
-    private Map<String, String> detailAttributes = new HashMap<String, String>();
+    //private Map<String, String> detailAttributes = new HashMap<String, String>();
+    //private Map<String, String> relationAttributes = new HashMap<String, String>();
+    //private Map<String, String> connections = new HashMap<String, String>();
 
-    public Record(Map<String, String> attributesT, Map<String, String> detailAttributesT) {
+    public Record(Map<String, String> attributesT) {
         this.attributes = attributesT;
-        this.detailAttributes = detailAttributesT;
     }
 
     public Record(Element recordElement) {
         getRecord(recordElement);
     }
-   
 
     public void getRecord(Element recordElement) {
-        /*String xmlString = "<record visibility=\"viewable\" visnote=\"logged in users\" selected=\"no\" depth=\"0\">\n"
-                + "    <!-- other XML elements here -->\n"
-                + "    <id>2533</id>\n"
-                + "    <type id=\"80\" conceptID=\"3-124\">Magazine</type>\n"
-                + "    <citeAs>https://heurist.sfb1288.uni-bielefeld.de/heurist/?recID=2533&amp;db=ipaetzold_erste_datenbank</citeAs>\n"
-                + "    <title>Schwuchtel - Eine Zeitung der Schwulenbewegung</title>\n"
-                + "    <added>2023-05-01 15:54:06</added>\n"
-                + "    <modified>2023-05-01 15:55:00</modified>\n"
-                + "    <workgroup id=\"0\">public</workgroup>\n"
-                + "    <detail conceptID=\"2-1\" name=\"Title of magazine\" id=\"1\" basename=\"Name or Title (please rename appropriately)\">Schwuchtel - Eine Zeitung der Schwulenbewegung</detail>\n"
-                + "    <detail conceptID=\"2-134\" name=\"Location (place)\" id=\"134\" basename=\"Location (place)\" isRecordPointer=\"true\">2263</detail>\n"
-                + "    <detail conceptID=\"3-1017\" name=\"Publisher &gt;\" id=\"1001\" basename=\"Publisher\" isRecordPointer=\"true\">2414</detail>\n"
-                + "    <detail conceptID=\"2-36\" id=\"36\" name=\"Original ID\" basename=\"Original ID\">0-2533</detail>\n"
-                + "</record>";
-        Map<String, String> attributes = new HashMap<String, String>();*/
 
         try {
-            /*DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(new InputSource(new StringReader(xmlString)));
-            Element recordElement = document.getDocumentElement();*/
-            
             NodeList nodeList = recordElement.getChildNodes();
-            //NodeList detailList = recordElement.getElementsByTagName("detail");
-
-
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     Element element = (Element) node;
                     String nodeName = element.getNodeName();
                     String nodeValue = element.getTextContent();
-                    nodeName=nodeName.replace(" ", "_").strip().stripLeading().stripTrailing().trim();
-                    //NodeList detailNodeList =element.getElementsByTagName("detail");
+                    nodeName = nodeName.replace(" ", "_").strip().stripLeading().stripTrailing().trim();
                     if (nodeName.contains("detail")) {
-                        detailAttributes = findDetail(element);
-                    } else {
-                        //System.out.println(nodeName+":"+nodeValue);
-                        attributes.put(nodeName, nodeValue);
-                    }
+                        findDetail(element,nodeName,nodeValue);
+                    } 
+                    else if (nodeName.contains("relationship")) {
+                        findRelation(element,nodeName,nodeValue);
+                    } 
+                  
+                    attributes.put(nodeName, nodeValue);
+                    
+                   
                 }
             }
-            //findDetail(detailList);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        /*for (String key : attributes.keySet()) {
-            String value = attributes.get(key);
-            System.out.println(key + " " + value);
-
-        }*/
+    }
+    
+    public void findRelation(Element relationElement, String nodeName, String nodeValue) {
+        attributes.put("relationshipTermID", relationElement.getAttribute("termID"));
+        attributes.put("relationshipRelatedRecordID", relationElement.getAttribute("relatedRecordID"));
+        attributes.put("relationshipTerm", relationElement.getAttribute("term"));
+        attributes.put("relationshipInverse", relationElement.getAttribute("inverse"));
+        attributes.put("relationshipInvTermID", relationElement.getAttribute("invTermID"));
+        attributes.put("relationshipInvTermConceptID", relationElement.getAttribute("invTermConceptID"));
+        attributes.put(nodeValue, nodeValue);
     }
 
-    public Map<String, String> findDetail(Element detailElementt) {
-        Map<String, String> detailAttributes=new HashMap<String, String>();
-        // String xmlString = "<detail conceptID=\"2-1\" name=\"Title of magazine\" id=\"1\" basename=\"Name or Title (please rename appropriately)\">Schwuchtel - Eine Zeitung der Schwulenbewegung</detail>";
-        //NodeList nodeList = detailElement.getChildNodes();
-        /*for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node = nodeList.item(i);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) node;
-                String nodeName = element.getNodeName();
-                String nodeValue = element.getTextContent();
-                nodeName = nodeName.replace(" ", "_").strip().stripLeading().stripTrailing().trim();
-                System.out.println("detail_"+nodeName+":"+nodeValue);
-                detailAttributes.put(nodeName, nodeValue);
-            }
-        }*/
-
-        try {
-            String conceptID = detailElementt.getAttribute("conceptID");
-            String name = detailElementt.getAttribute("name");
-            String id = detailElementt.getAttribute("id");
-            String basename = detailElementt.getAttribute("basename");
-            String textContent = detailElementt.getTextContent();
-            detailAttributes.put("conceptID", conceptID);
-            detailAttributes.put("name", name);
-            detailAttributes.put("basename", basename);
-            detailAttributes.put("textContent", textContent);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return detailAttributes;
+    public void findDetail(Element detailElementt, String nodeName, String nodeValue) {
+        attributes.put("detailId", detailElementt.getAttribute("id"));
+        attributes.put("detailConceptID", detailElementt.getAttribute("conceptID"));
+        attributes.put("detailName", detailElementt.getAttribute("name"));
+        attributes.put("detailBasename", detailElementt.getAttribute("basename"));
+        attributes.put(nodeName, nodeValue);
     }
 
     public Map<String, String> getAttributes() {
         return attributes;
     }
 
-    public Map<String, String> getDetailAttributes() {
+    /*public Map<String, String> getDetailAttributes() {
         return detailAttributes;
     }
+
+    public Map<String, String> getRelationAttributes() {
+        return relationAttributes;
+    }*/
+
+   
+    
 
 }
